e9d19a3a28538ebdeee9f2cde7f46bd52b8ac5a8
Rene Presedo <r.presedo.ext@sonnen.de>
2024-04-04 08:16:20 +0200

    Added borders to tokyonight

--- vim/lua/plugins/colorscheme.lua
+++ vim/lua/plugins/colorscheme.lua
@@ -2,6 +2,16 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "night" },
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_colors = function(colors)
+        colors.border = "#565f89"
+      end,
+    },
   },
 }
