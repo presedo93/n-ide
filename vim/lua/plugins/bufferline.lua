8c6033e653693a2a857ae3b3a99d6891f3476748
Rene Presedo <r.presedo.ext@sonnen.de>
2024-04-09 16:46:18 +0200

    Zellij run on floating a command

--- vim/lua/plugins/bufferline.lua
+++ vim/lua/plugins/bufferline.lua
@@ -3,5 +3,6 @@ return {
   event = "VeryLazy",
   opts = function(_, opts)
     opts.options.show_buffer_close_icons = false
+    opts.options.separator_style = { "", "" }
   end,
 }
--- vim/lua/plugins/colorscheme.lua
+++ vim/lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
       transparent = true,
       styles = {
         sidebars = "transparent",
-        floats = "transparent",
+        -- floats = "transparent",
       },
       on_colors = function(colors)
         colors.border = "#565f89"
