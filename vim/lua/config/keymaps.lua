fd08222019fc2055d63aeda027c024eb006b2e51
Rene Presedo <presedo93@gmail.com>
2024-10-18 17:00:37 +0200

    feat: allow embed and full screen pane command

--- vim/lua/config/keymaps.lua
+++ vim/lua/config/keymaps.lua
@@ -2,13 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without copying" })
-
 vim.keymap.set("n", "<S-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 vim.keymap.set("n", "<S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 vim.keymap.set("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 vim.keymap.set("n", "U", "<C-r>", { desc = "Re-do" })
-
 vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>", { desc = "Toggle Zen Mode" })
