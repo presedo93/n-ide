77cbb812e475211ca8a1f1bb32c4d0377db9d3ce
Rene Presedo <presedo93@gmail.com>
2024-11-27 23:13:04 +0100

    fix: update vim plugins

--- vim/lua/plugins/bufferline.lua
+++ vim/lua/plugins/bufferline.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/bufferline.nvim",
   event = "VeryLazy",
   opts = function(_, opts)
-    opts.options.show_buffer_close_icons = false
+    opts.options.show_buffer_close_icons = true
     opts.options.separator_style = { "", "" }
   end,
 }
--- vim/lua/plugins/lualine.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  opts = function(_, opts)
-    opts.sections.lualine_z = {}
-    opts.options.theme = "auto"
-  end,
-}
