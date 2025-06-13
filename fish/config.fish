0030abe9876f5bccd5bb655e2d46b72384864345
Rene Presedo <r.presedo@Renes-MacBook-Air.local>
2025-06-13 08:33:01 +0200

    fix: add time section to starship

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,7 @@ alias zlj='zellij'
 
 pyenv init - fish | source
 starship init fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
