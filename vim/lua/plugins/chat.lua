b60cbf220fb54f33c50a8e5240f89da86b8a08ab
Rene Presedo <r.presedo@Renes-MacBook-Air.local>
2025-08-25 17:34:04 +0200

    feat: add copilot-chat config

---
+++ vim/lua/plugins/chat.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = "gpt-4o", -- AI model to use -> default: gpt-4.1
+      temperature = 0.6, -- Lower = focused, higher = creative
+    },
+  },
+}
