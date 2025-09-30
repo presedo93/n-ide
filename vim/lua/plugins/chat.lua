62cad06a5e7c1a0559ff6c15e38fb94393120faa
Rene Presedo <presedo93@gmail.com>
2025-09-30 08:52:10 +0200

    fix: add sidekick keybindings

--- vim/lua/plugins/chat.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    opts = {
-      -- model = "gpt-4o", -- AI model to use -> default: gpt-4.1
-      temperature = 0.6, -- Lower = focused, higher = creative
-    },
-  },
-}
