aa6500c3f9cb1ce0789c5f2a45a8a65d318b7dad
Rene Presedo <presedo93@gmail.com>
2025-10-16 19:23:19 +0200

    feat: update keymaps

--- vim/lua/config/keymaps.lua
+++ vim/lua/config/keymaps.lua
@@ -8,4 +8,8 @@ vim.keymap.set("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decreas
 vim.keymap.set("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 vim.keymap.set("n", "U", "<C-r>", { desc = "Re-do" })
-vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>", { desc = "Toggle Zen Mode" })
+
+vim.keymap.set({ "n", "x" }, "d", '"_d', { noremap = true, silent = true, desc = "Delete without yanking" })
+vim.keymap.set("n", "dd", '"_dd', { noremap = true, silent = true, desc = "Delete line without yanking" })
+
+vim.keymap.set("t", "<S-Right>", function() Snacks.terminal(nil, { win = { split = "right" } }) end, { desc = "Right split terminal" })
