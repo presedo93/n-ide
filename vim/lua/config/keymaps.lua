cdf13647861072e9d77b0ba225cc3c777050cc9a
Rene Presedo <presedo93@gmail.com>
2026-03-03 16:01:11 +0100

    feat(lazy): add keymaps for lazygit

--- vim/lua/config/keymaps.lua
+++ vim/lua/config/keymaps.lua
@@ -12,4 +12,14 @@ vim.keymap.set("n", "U", "<C-r>", { desc = "Re-do" })
 vim.keymap.set({ "n", "x" }, "d", '"_d', { noremap = true, silent = true, desc = "Delete without yanking" })
 vim.keymap.set("n", "dd", '"_dd', { noremap = true, silent = true, desc = "Delete line without yanking" })
 
-vim.keymap.set("t", "<S-Right>", function() Snacks.terminal(nil, { win = { split = "right" } }) end, { desc = "Right split terminal" })
+vim.keymap.set("t", "<S-Right>", function()
+  Snacks.terminal(nil, { win = { split = "right" } })
+end, { desc = "Right split terminal" })
+
+vim.keymap.set("n", "<C-g>", function()
+  Snacks.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Open lazygit" })
+
+vim.keymap.set("t", "<C-g>", function()
+  Snacks.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Open lazygit" })
